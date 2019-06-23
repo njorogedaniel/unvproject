4e6da0d4c218dd7b6f2dcab46c6d2b22d8842caa
njorogedaniel <dnjoroge96.dn@gmail.com>
2019-06-23 17:43:32 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+require ('config.php');
+if (isset($_POST['search'])){
+    $id = $_POST['search'];
+    $sql = "SELECT * FROM projects WHERE project_id='$id'";
+    if ($link->query($sql) == true){
+
+
+    }
+}
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -25,7 +38,80 @@
             padding-bottom: 10px;
 
         }
+         * {box-sizing: border-box;}
 
+        body {
+            margin: 0;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+
+        .topnav {
+            overflow: hidden;
+            background-color: #e9e9e9;
+        }
+
+        .topnav a {
+            float: left;
+            display: block;
+            color: black;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+            font-size: 17px;
+        }
+
+        .topnav a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+
+        .topnav a.active {
+            background-color: #2196F3;
+            color: white;
+        }
+
+         .search-container {
+            float: right;
+        }
+
+        .topnav input[type=text] {
+            padding: 6px;
+            margin-top: 8px;
+            font-size: 17px;
+            border: none;
+        }
+
+        .topnav .search-container button {
+            float: right;
+            padding: 6px 10px;
+            margin-top: 8px;
+            margin-right: 16px;
+            background: #ddd;
+            font-size: 17px;
+            border: none;
+            cursor: pointer;
+        }
+
+        .topnav .search-container button:hover {
+            background: #ccc;
+        }
+
+        @media screen and (max-width: 600px) {
+            .topnav .search-container {
+                float: none;
+            }
+            .topnav a, .topnav input[type=text], .topnav .search-container button {
+                float: none;
+                display: block;
+                text-align: left;
+                width: 100%;
+                margin: 0;
+                padding: 14px;
+            }
+            .topnav input[type=text] {
+                border: 1px solid #ccc;
+            }
+        }
     </style>
 </head>
 <body>
@@ -52,10 +138,15 @@
             <ul class="nav navbar-nav">
                 <li class="active"><a href="index.php">Home</a></li>
                 <li><a href="projects.php">Projects</a></li>
-                <li><a href="#">Page 2</a></li>
             </ul>
+            <div class="search-container">
+                <form action="projectDisplay.php"method="get">
+                    <input type="number" placeholder="Search.." name="search">
+                    <button type="submit"><span class="glypicon glyphicon_finder">Submit</span></button>
+                </form>
+            </div>
         </div>
     </nav>
-    <p> This page should display a map that  shows the distribution of projects worldwide</p>
+    <a href="https://davannjoro.maps.arcgis.com/home/webmap/viewer.html?webmap=183e03dd8cdb4cf383e799e3902dbba5">View Projects Distribution</a>
 </body>
 </html>
